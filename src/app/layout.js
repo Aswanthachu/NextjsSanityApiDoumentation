a59c4698e01d48637768e023dc58ddbf888f76c2
Aswanthachu <pnkaswanthachu546@gmail.com>
2023-09-18 15:16:14 +0530

    hydration warning removed.

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,10 +8,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="flex" suppressHydrationWarning={true}>
+    <html lang="en" suppressHydrationWarning>
+      <body className="flex" >
           <SideBar />
-          <div className="w-full ml-[250px]" suppressHydrationWarning={true}>{children}</div>
+          <div className="w-full ml-[250px]">{children}</div>
       </body>
     </html>
   );
