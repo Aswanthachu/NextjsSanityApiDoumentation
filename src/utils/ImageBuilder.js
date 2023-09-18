1dedb5122c58cd61758814b6b2494ccedea4c3ef
Aswanthachu <pnkaswanthachu546@gmail.com>
2023-09-18 16:09:00 +0530

    .env is added.

--- src/utils/ImageBuilder.js
+++ src/utils/ImageBuilder.js
@@ -1,10 +1,8 @@
 import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder({
-  projectId: '3bjwcw6a',
-  dataset: 'production',
-  // projectId: process.env.PROJECT_ID,
-  // dataset: process.env.DATASET,
+  projectId: `${process.env.PROJECT_ID}`,
+  dataset: `${process.env.DATASET}`,
   baseUrl: "https://cdn.sanity.io",
 });
 
--- src/utils/configSanity.js
+++ src/utils/configSanity.js
@@ -1,14 +1,8 @@
 import { createClient } from "next-sanity";
 
-// const PROJECT_ID = process.env.PROJECT_ID
-// const DATASET = process.env.DATASET
-
-
 export const client = createClient({
-  // projectId: PROJECT_ID,
-  // dataset: DATASET,
-  projectId: '3bjwcw6a',
-  dataset: 'production',
+  projectId: `${process.env.PROJECT_ID}`,
+  dataset: `${process.env.DATASET}`,
   apiVersion: "2021-10-21",
   useCdn: true,
 });
